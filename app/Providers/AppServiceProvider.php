5b13e4516450ceb43fa96cf7a74f6388b9fcab0b
Jeg Ramos <jegramos.pa@gmail.com>
2022-11-03 20:23:09 +0800

    :construction: Base WIP

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        /**
+         * Load IDE helper for non-production environment
+         * @see https://github.com/barryvdh/laravel-ide-helper
+         */
+        if ($this->app->isLocal()) {
+            $this->app->register(IdeHelperServiceProvider::class);
+        }
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_11_03_104429_create_user_profiles_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->unique()->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('middle_name')->nullable();
+            $table->string('mobile_number')->nullable();
+            $table->string('telephone_number')->nullable();
+            $table->enum('sex', ['make', 'female'])->nullable();
+            $table->date('birthday')->nullable();
+            // Address columns are created in a way that will enable us to work with multiple countries
+            $table->string('address_line_1')->nullable(); // Building number, Building name
+            $table->string('address_line_2')->nullable(); // Street, Road name
+            $table->string('address_line_3')->nullable(); // Additional address info
+            $table->string('district')->nullable(); // Barangay, etc.
+            $table->string('city')->nullable(); // or Municipality
+            $table->string('province')->nullable();
+            $table->string('postal_code')->nullable();
+            $table->string('profile_picture_url')->nullable();
+            $table->string('country')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_profiles');
+    }
+};
