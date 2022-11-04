eb9eba493adf716983d38ca61f32b8a4ddb90315
jegramos <jegramos.pa@gmail.com>
2022-11-04 11:59:45 +0800

    :test_tube: test

--- app/Console/Commands/StyleFixer.php
+++ app/Console/Commands/StyleFixer.php
@@ -56,7 +56,7 @@ class StyleFixer extends Command
             }
         }
 
-        $this->info('🧺 Code cleanup done!');
+        $this->info('🧺 Code cleanup done');
 
         return $exitCode;
     }
