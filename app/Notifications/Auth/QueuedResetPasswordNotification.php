a5822217dd8c08561acbc358cd51c566bcf70e2e
jegramos <jegramos.pa@gmail.com>
2022-12-06 21:05:17 +0800

    :recycle: refactor

--- app/Notifications/Auth/QueuedResetPasswordNotification.php
+++ app/Notifications/Auth/QueuedResetPasswordNotification.php
@@ -26,7 +26,8 @@ class QueuedResetPasswordNotification extends ResetPassword implements ShouldQue
                 'You are receiving this email because we received a password reset request for your account.'
             ))
             ->action(Lang::get('Reset Password'), $url)
-            ->line(Lang::get('This password reset link will expire in :count minutes.',
+            ->line(Lang::get(
+                'This password reset link will expire in :count minutes.',
                 ['count' => config('auth.passwords.' . config('auth.defaults.passwords') . '.expire')]
             ))
             ->line(Lang::get('If you did not request a password reset, no further action is required.'));
