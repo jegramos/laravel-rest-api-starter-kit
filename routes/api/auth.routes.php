c9c0744d2b6cc048a55a6a5f4fc489ffe0fcc0f0
jegramos <jegramos.pa@gmail.com>
2023-01-25 17:51:33 +0800

    :bug: Relative path signature validation

--- routes/api/auth.routes.php
+++ routes/api/auth.routes.php
@@ -34,7 +34,7 @@ Route::controller(AuthController::class)->group(function () {
         ->name('auth.verification.resend');
 
     /** @uses AuthController::verifyEmail */
-    Route::middleware(['signed'])
+    Route::middleware(['auth:sanctum', 'signed:relative'])
         ->get('email/verify/{id}/{hash}', 'verifyEmail')
         ->name('verification.verify');
 });
